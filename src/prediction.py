cac61a10d9e66e02355afcadf6535e269feecb9f
yusufali98 <yusufali.dtu.98@gmail.com>
2021-11-20 03:45:20 +0530

    prediction.py updated

--- src/prediction.py
+++ src/prediction.py
@@ -1,23 +1,55 @@
 import tensorflow
 from PIL import Image, ImageOps
 import numpy as np
+import tqdm
+import os
+
+test_data_folder = '/home/yusuf/Desktop/CS7785/Lab_6/lab6_ws/src/team19_lab6/test_images/'
 
 model = tensorflow.keras.models.load_model('/home/yusuf/Desktop/CS7785/Lab_6/lab6_ws/src/team19_lab6/keras_model.h5', compile=False)
 
-data = np.ndarray(shape = (1,224,224,3), dtype = np.float32)
+print(os.listdir(test_data_folder))
 
-image = Image.open('/home/yusuf/Desktop/CS7785/Lab_6/lab6_ws/src/team19_lab6/test_images/10.jpg')
+idx = 1
 
-size = (224,224)
+pred_list = []
 
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
+for files in (os.listdir(test_data_folder)):
 
-image_array = np.asarray(image)
+    fname = test_data_folder + files
 
-normalized_image_arr = (image_array.astype(np.float32) / 127.0) - 1
+    print(fname)
 
-data[0] = normalized_image_arr
+    data = np.ndarray(shape = (1,224,224,3), dtype = np.float32)
 
-prediction = model.predict(data)
+    image = Image.open(fname)
 
-print("\n Prediction: ", prediction)
+    size = (224,224)
+
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    image_array = np.asarray(image)
+
+    normalized_image_arr = (image_array.astype(np.float32) / 127.0) - 1
+
+    data[0] = normalized_image_arr
+
+    prediction = model.predict(data)
+
+    prediction_idx = np.argmax(prediction) + 1
+
+    if prediction_idx == 6:
+        prediction_idx = 0
+
+    print("\n Prediction ", fname, " : ", prediction_idx)
+
+    pred_list.append(prediction_idx)
+
+    idx += 1
+
+print(pred_list)
+
+pred_list = np.asarray(pred_list)
+
+print(pred_list)
+
